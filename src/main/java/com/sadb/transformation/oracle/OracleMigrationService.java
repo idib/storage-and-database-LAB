c749fbaee390471a69205fb77994c3aa63369c18
Pavel <moshguuy@gmail.com>
2019-03-24 02:15:21 +0300

    Сhanged the length of FAC_NAME in the table FACULTY on 150
    Add method processFaculty

--- src/main/java/com/sadb/transformation/oracle/OracleMigrationService.java
+++ src/main/java/com/sadb/transformation/oracle/OracleMigrationService.java
@@ -1,7 +1,9 @@
 package com.sadb.transformation.oracle;
 
+import com.sadb.generated.source.oracle.tables.Faculty;
 import com.sadb.generated.source.oracle.tables.Lecturer;
 import com.sadb.generated.source.oracle.tables.Megafaculty;
+import com.sadb.generated.source.oracle.tables.records.FacultyRecord;
 import com.sadb.generated.source.oracle.tables.records.LecturerRecord;
 import com.sadb.generated.source.oracle.tables.records.MegafacultyRecord;
 import com.sadb.transformation.ConnectionManager;
@@ -61,7 +63,7 @@ public class OracleMigrationService {
 //        );
 
         // LECTURER LECTURER LECTURER LECTURER LECTURER LECTURER LECTURER
-        Result<LecturerRecord> oracleSourceLecturer =
+/*        Result<LecturerRecord> oracleSourceLecturer =
                 contextSourceOracle.select().from(Lecturer.LECTURER).fetch().into(Lecturer.LECTURER);
 
         Result<com.sadb.generated.dest.oracle.tables.records.LecturerRecord> oracleLectures =
@@ -71,6 +73,39 @@ public class OracleMigrationService {
         processOracleLecturer(
                 oracleSourceLecturer,
                 oracleLectures,
+                toInsert,
+                toUpdate
+        );*/
+/*
+
+        // STUDENTS STUDENTS STUDENTS STUDENTS STUDENTS STUDENTS STUDENTS STUDENTS
+
+        Result<StudentRecord> oracleSourceStudents =
+                contextOracle.select().from(Student.STUDENT).fetch().into(Student.STUDENT);
+
+        Result<com.sadb.generated.dest.oracle.tables.records.StudentRecord> oracleStudents =
+                contextOracle.select().from(com.sadb.generated.dest.oracle.tables.Student.STUDENT).fetch().into(com.sadb.generated.dest.oracle.tables.Student.STUDENT);
+
+
+        processOracleStudents(
+                oracleSourceStudents,
+                oracleStudents,
+                toInsert,
+                toUpdate
+        );
+*/
+
+        //  FACULTY FACULTY FACULTY FACULTY FACULTY
+
+        Result<FacultyRecord> oracleSourseFaculty =
+                contextSourceOracle.select().from(Faculty.FACULTY).fetch().into(Faculty.FACULTY);
+
+        Result<com.sadb.generated.dest.oracle.tables.records.FacultyRecord> oracleFaculty =
+                contextOracle.select().from(com.sadb.generated.dest.oracle.tables.Faculty.FACULTY).fetch().into(com.sadb.generated.dest.oracle.tables.Faculty.FACULTY);
+
+        processFaculty(
+                oracleSourseFaculty,
+                oracleFaculty,
                 toInsert,
                 toUpdate
         );
@@ -102,7 +137,7 @@ public class OracleMigrationService {
 
         for (MegafacultyRecord oracleMegafacultyRecord : oracleSourceMegafaculty) {
 
-            // условие на нулпоинтер
+
             Timestamp oracleSourceRecordUpdateDate = new Timestamp(oracleMegafacultyRecord.getUpdateTime().getTime());
             Timestamp oracleRecordUpdateDate = megafacultyIdToUpdatedDateMap.get(oracleMegafacultyRecord.getMegafacId().intValue());
 
@@ -189,6 +224,73 @@ public class OracleMigrationService {
                     toInsert.add(lecturerRecord);
                 } else if (oracleSourceRecordUpdateDate.after(oracleRecordUpdateDate)) {
                     toUpdate.add(lecturerRecord);
+                }
+            }
+
+        }
+
+    }
+
+/*    private void processOracleStudents(
+            List<StudentRecord> oracleSourceStudents,
+            List<com.sadb.generated.dest.oracle.tables.records.StudentRecord> oracleStudents,
+            List<TableRecord<?>> toInsert,
+            List<UpdatableRecord<?>> toUpdate) {
+
+        Map<Integer, Timestamp> studentIdToUpdatedDateMap = new HashMap<>();
+        Map<Integer, com.sadb.generated.dest.oracle.tables.records.StudentRecord> studentIdToRecordMap = new HashMap<>();
+
+        oracleStudents.forEach(oracleStudent -> {
+            studentIdToUpdatedDateMap.put(oracleStudent.getId().intValue(), oracleStudent.getUpdationDate());
+            studentIdToRecordMap.put(oracleStudent.getId().intValue(), oracleStudent);
+        });
+
+    }*/
+
+    private void processFaculty(
+            List<FacultyRecord> oracleSourceFacultys,
+            List<com.sadb.generated.dest.oracle.tables.records.FacultyRecord> oracleFacultys,
+            List<TableRecord<?>> toInsert,
+            List<UpdatableRecord<?>> toUpdate) {
+
+        Map<Integer, Timestamp> facultyIdToUpdatedDateMap = new HashMap<>();
+        Map<Integer, com.sadb.generated.dest.oracle.tables.records.FacultyRecord> facultyIdToRecordMap = new HashMap<>();
+
+        oracleFacultys.forEach(oracleFaculty -> {
+            facultyIdToUpdatedDateMap.put(oracleFaculty.getFacId().intValue(), oracleFaculty.getUpdateTime());
+            facultyIdToRecordMap.put(oracleFaculty.getFacId().intValue(), oracleFaculty);
+        });
+
+        for (FacultyRecord oracleFacultyRecord : oracleSourceFacultys) {
+
+
+            Timestamp oracleSourceRecordUpdateDate = new Timestamp(oracleFacultyRecord.getUpdateTime().getTime());
+            Timestamp oracleRecordUpdateDate = facultyIdToUpdatedDateMap.get(oracleFacultyRecord.getFacId().intValue());
+
+            if (oracleRecordUpdateDate == null || oracleSourceRecordUpdateDate.after(oracleRecordUpdateDate)) {
+
+                com.sadb.generated.dest.oracle.tables.records.FacultyRecord oldRecord =
+                        facultyIdToRecordMap.get(oracleFacultyRecord.getFacId().intValue());
+
+                com.sadb.generated.dest.oracle.tables.records.FacultyRecord facultyRecord;
+
+                if (oldRecord == null) {
+                    facultyRecord = new com.sadb.generated.dest.oracle.tables.records.FacultyRecord();
+                } else {
+                    facultyRecord = oldRecord;
+                    facultyRecord.changed(true);
+                }
+
+                facultyRecord.setFacId(oracleFacultyRecord.getFacId().longValue());
+                facultyRecord.setFacName(oracleFacultyRecord.getFacName());
+                facultyRecord.setMegafacId(oracleFacultyRecord.getMegafacId().longValue());
+                facultyRecord.setCreatTime(new Timestamp(oracleFacultyRecord.getCreatTime().getTime()));
+                facultyRecord.setUpdateTime(new Timestamp(oracleFacultyRecord.getUpdateTime().getTime()));
+
+                if (oracleRecordUpdateDate == null) {
+                    toInsert.add(facultyRecord);
+                } else if (oracleSourceRecordUpdateDate.after(oracleRecordUpdateDate)) {
+                    toUpdate.add(facultyRecord);
                 }
             }
 
